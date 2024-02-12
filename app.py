fb46f5980bba48a0f9eeb3802570fccb05b0f48b
Chloe Fox <cfox_01@arcadia.edu>
2024-02-12 11:30:22 -0500

    Add llm connection

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
+from langchain_community.chat_models import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+
 import chainlit as cl
 
 
-@cl.on_message
-async def main(message: cl.Message):
-    # Your custom logic goes here...
+@cl.on_chat_start
+async def on_chat_start():
+    model = ChatOllama(model="llama2:7b")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
 
-    # Send a response back to the user
-    await cl.Message(
-        content=f"Received: {message.content}",
-    ).send()
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
 
