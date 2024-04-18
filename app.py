ea55dcf174b56ca8f57945b05b857686245d294b
Chloe Fox <cfox_01@arcadia.edu>
2024-04-17 21:12:49 -0400

    API working, but not with chat

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 from langchain_community.chat_models import ChatOllama
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
@@ -29,6 +30,11 @@ async def on_chat_start():
 async def on_message(message: cl.Message):
     runnable = cl.user_session.get("runnable")  # type: Runnable
 
+    # Ask the user if they want to make an API call
+    api_call_response = input("Do you want to make an API call? (yes/no): ")
+    if api_call_response.lower() == "yes":
+        make_api_calls()
+
     msg = cl.Message(content="")
 
     async for chunk in runnable.astream(
@@ -39,3 +45,52 @@ async def on_message(message: cl.Message):
 
     await msg.send()
 
+def make_api_calls():
+    def make_api_call(api_data):
+        url = "http://localhost:8080/api/user.php"  # Replace this with your actual API endpoint
+        headers = {"Content-Type": "application/json"}
+
+        response = requests.post(url, json=api_data, headers=headers)
+        
+        if response.status_code == 200:
+            print("API call successful!")
+            print("Response:", response.json())
+        else:
+            print("API call failed with status code:", response.status_code)
+            print("Response:", response.text)
+
+    # Example API calls
+    api_calls = [
+        {
+            "section": "file",
+            "request": "addFile",
+            "filename": "doesnt-matter1.txt",
+            "fileType": 0,
+            "source": "url",
+            "accessGroupId": 1,
+            "data": "https://github.com/kkrypt0nn/wordlists/blob/main/wordlists/passwords/common_passwords_win.txt",
+            "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+        },
+        {
+            "section": "file",
+            "request": "addFile",
+            "filename": "doesnt-matter1.txt",
+            "fileType": 0,
+            "source": "url",
+            "accessGroupId": 1,
+            "data": "https://github.com/stealthsploit/OneRuleToRuleThemStill/blob/main/OneRuleToRuleThemStill.rule",
+            "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+        },
+        
+        {
+            "section": "file",
+            "request": "listFiles",
+            "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+        },
+        # Add the rest of your API calls here...
+    ]
+
+    # Make each API call
+    for api_call in api_calls:
+        make_api_call(api_call)
+        time.sleep(10)
