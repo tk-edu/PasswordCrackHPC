f0a44fac3e6771387783ab2305d379a98c655183
Arjun Fulp <arjunfulp@gmail.com>
2024-04-19 23:41:57 -0400

    Break down api calls

--- app.py
+++ app.py
@@ -13,6 +13,28 @@ wordlist = ""
 rulelist = ""
 hashlist = ""
 
+file_data_1 = {
+    "section": "file",
+    "request": "addFile",
+    "filename": "doesnt-matter1.txt",
+    "fileType": 0,
+    "source": "url",
+    "accessGroupId": 1,
+    "data": "https://github.com/kkrypt0nn/wordlists/blob/main/wordlists/passwords/common_passwords_win.txt",
+    "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+    }
+
+file_data_2 = {
+    "section": "file",
+    "request": "addFile",
+    "filename": "doesnt-matter1.txt",
+    "fileType": 1,
+    "source": "url",
+    "accessGroupId": 1,
+    "data": "https://github.com/stealthsploit/OneRuleToRuleThemStill/blob/main/OneRuleToRuleThemStill.rule",
+    "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+}
+
 @cl.on_chat_start
 async def on_chat_start():
     # Change model here
@@ -26,14 +48,14 @@ async def on_chat_start():
             ("human", "{question}"),
         ]
     )
-    
+
     runnable = prompt | model | StrOutputParser()
     cl.user_session.set("runnable", runnable)
 
 @cl.on_message
 async def on_message(message: cl.Message):
     runnable = cl.user_session.get("runnable")  # type: Runnable
-    
+
     msg = cl.Message(content="")
 
     # Check if the user wants to make an API call
@@ -51,42 +73,99 @@ async def on_message(message: cl.Message):
         await msg.send()
 
 def make_api_calls():
-        
+
     def make_api_call(api_data):
         url = "http://localhost:8080/api/user.php"  # Replace this with your actual API endpoint
         headers = {"Content-Type": "application/json"}
 
         response = requests.post(url, json=api_data, headers=headers)
-        
+
         if response.status_code == 200:
             print("API call successful!")
             print("Response:", response.json())
-            # Check if the API call is for listing files and save the fileId accordingly
-            if api_data.get('request') == 'listFiles':
-                for file_info in response.json().get('files', []):
-                    if file_info['fileType'] == 0:
-                        wordlist = file_info['fileId']
-                        print(wordlist)
-                    elif file_info['fileType'] == 1:
-                        rulelist = file_info['fileId']
-                        print(rulelist)
-            # Check if the API call is for creating a hashlist and save the hashlistId accordingly
-            elif api_data.get('request') == 'createHashlist':
-                hashlist = response.json().get('hashlistId')
-                print(hashlist)
-            elif api_data.get('request') == 'createTask':
-                task_id = response.json().get('taskId')
-                print(task_id)
-            elif api_data.get('request') == 'getTask':
-                if response.json().get('isComplete') == False:
-                    print("Sleepy time")
-                    time.sleep(10)
-                    make_api_call(api_data)
-                    
-                
+
+            return response.json();
+
         else:
             print("API call failed with status code:", response.status_code)
             print("Response:", response.text)
+
+
+    def create_hashlist():
+        api_data = {
+            "section": "hashlist",
+            "request": "createHashlist",
+            "name": "API Hashlist",
+            "isSalted": False,
+            "isSecret": True,
+            "isHexSalt": False,
+            "separator": ":",
+            "format": 0,
+            "hashtypeId": 0,
+            "accessGroupId": 1,
+            "data": "YWFhCmFiYwphY2FkZW1pYQphY2FkZW1pYwphY2Nlc3MKYWRhCmFkbWluCmFkcmlhbgphZHJpYW5uYQphZXJvYmljcwphaXJwbGFuZQphbGJhbnk=",
+            "useBrain": False,
+            "brainFeatures": 0,
+            "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+        },
+
+        hashlistId = make_api_call(api_data);
+        print('-------------');
+        print("Hashlist ID:", hashlistId);
+        print('-------------');
+        return hashlistId
+
+    def add_file(api_data):
+        return make_api_call(api_data);
+
+    def list_files():
+        api_data = {
+            "section": "file",
+            "request": "listFiles",
+            "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+        }
+
+        files = make_api_call(api_data);
+
+        print('-------------');
+        print("List of Files:");
+        print(files);
+        print('-------------');
+
+        return files
+
+    def create_task_data(files, hashlistId):
+        return {
+            "section": "task",
+            "request": "createTask",
+            "name": "API Task",
+            "hashlistId": hashlistId,
+            "attackCmd": "#HL# -a 0 -r OneRuleToRuleThemStill.rule common_passwords_win.txt",
+            "chunksize": 600,
+            "statusTimer": 5,
+            "benchmarkType": "speed",
+            "color": "FF4AAB",
+            "isCpuOnly": True,
+            "isSmall": True,
+            "skip": 0,
+            "crackerVersionId": 1,
+            "files": [
+                files[0]['id'],
+                files[1]['id']
+            ],
+            "priority": 1,
+            "maxAgents": 1,
+            "preprocessorId": 0,
+            "preprocessorCommand": "",
+            "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
+        }
+
+    def create_task(api_data):
+        taskId = make_api_call(api_data);
+        print('-------------');
+        print("Task ID:", taskId);
+        print('-------------');
+        return taskId
 
     # Example API calls
     api_calls = [
@@ -110,13 +189,13 @@ def make_api_calls():
             "data": "https://github.com/stealthsploit/OneRuleToRuleThemStill/blob/main/OneRuleToRuleThemStill.rule",
             "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
         },
-        
+
         {
             "section": "file",
             "request": "listFiles",
             "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
         },
-        
+
         {
             "section": "hashlist",
             "request": "createHashlist",
@@ -160,7 +239,7 @@ def make_api_calls():
         {
             "section": "task",
             "request": "taskAssignAgent",
-            "agentId": 6,  
+            "agentId": 6,
             "taskId": 17,  # Replace this with the actual taskId
             "accessKey": "am1wGeToLAhrlpWErAtxDzXXGsj8s1"
         },
@@ -179,10 +258,14 @@ def make_api_calls():
         # Add the rest of your API calls here...
     ]
 
-    # Make each API call
-    for api_call in api_calls:
-        make_api_call(api_call)
-        time.sleep(5)
-    
-    
+    add_file(file_data_1);
+    add_file(file_data_2);
+    files = list_files();
+    hashlistId = create_hashlist();
+    taskData = create_task_data(files, hashlistId);
+    taskId = create_task(taskData);
+
+    print(hashlistId);
+    print(taskId);
+
     return "All API calls completed."
